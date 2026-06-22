548a3e7039c1264f0b6acb34855348ab7defb1cb
liamlalazry <liamlalasri@gmail.com>
2026-06-22 09:14:54 +0300

    Add example of race condition in multithreading
    This code demonstrates a race condition with multiple threads incrementing a shared variable.

---
+++ RaceConditionExample.cpp
@@ -0,0 +1,34 @@
+#include <Windows.h>
+#include <stdio.h>
+#define NUM_THREADS 2
+#define MAX 100000
+VOID inc(PINT param) {
+	INT temp = *param;
+	temp++;
+	*param = temp;
+}
+DWORD WINAPI thread_func(LPVOID param) {
+	for (INT i = 0; i < MAX; i++) {
+		inc((PINT)param);
+	}
+	return 1;
+}
+int main()
+{
+	INT val = 0;
+	LPVOID param = &val;
+	HANDLE hThread[NUM_THREADS];
+	for (INT i = 0; i < NUM_THREADS; i++) {
+		hThread[i] = CreateThread(
+			NULL, //default security attributes
+			0, //default stack size
+			thread_func,//thread function
+			param, //thread param
+			0, //default creation flags
+			NULL //return thread identifier
+		);
+	}
+	WaitForMultipleObjects(NUM_THREADS, hThread, TRUE, INFINITE);
+	printf("Param value: %d\n", *(PINT)param);
+}
+
